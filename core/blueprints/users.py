1b319645c2bca8a8449bba7bd22f2f901b05928e
Jonathan <machadofjonathan@gmail.com>
2024-02-13 09:23:16 +0000

    Remove get_all_users implementation

--- core/blueprints/users.py
+++ core/blueprints/users.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify
-from core.models import User
 from core.schemas import UserSchema
 
 users_api = Blueprint("users_api", __name__)
@@ -7,6 +6,6 @@ users_api = Blueprint("users_api", __name__)
 
 @users_api.route("/users", methods=["GET"])
 def get_all_users():
-    all_users = User.select()
-    data = UserSchema().dump(all_users, many=True)
-    return jsonify({"data": data, "count": len(data)})
+    # ...
+    # return jsonify({"data": data, "count": len(data)})
+    raise NotImplementedError
